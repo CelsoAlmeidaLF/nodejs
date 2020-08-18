115b53b2e414c78d60ef68317bb1c6ac9b9c0599
CelsoAlmeidaLF <celso.almeida.leite@hotmail.com>
2020-08-18 18:56:16 -0300

    Atualização do projeto.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,32 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: '',
+  entry: './src/js/index.js',
   output: {
-    filename:'',
-    path: path.resolve(''),
-  }
-}
+    filename:'scripts/main.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+      },
+      {
+        test:/.\.js$/,
+        exclude:/node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: { presets:['@babel/preset-env'] }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles/main.css'
+    })
+  ]
+};
